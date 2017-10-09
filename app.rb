db92f9851c01b3614960c367e667dc3c9509971b
gene-yoo <gene.y.yoo@gmail.com>
2017-10-09 18:46:47 -0400

    Lab complete.

--- app.rb
+++ app.rb
@@ -2,5 +2,49 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
 
+  get '/square/:number' do
+    @num = params[:number]
+    "#{@num.to_i**2}"
+  end
+
+  get '/say/:number/:phrase' do
+    @num = params[:number]
+    @phrase = params[:phrase]
+
+    text = []
+
+    @num.to_i.times do
+      text << @phrase
+    end
+
+    "#{text.join(" ")}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @words = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]]
+    "#{@words.join(" ")}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @op = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    case @op
+    when "add"
+      "#{@num1 + @num2}"
+    when "subtract"
+      "#{@num1 - @num2}"
+    when "multiply"
+      "#{@num1 * @num2}"
+    when "divide"
+      "#{@num1 / @num2}"
+    else
+      "Invalid."
+    end
+  end
 end
